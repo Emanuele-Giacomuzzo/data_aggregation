696eed8b7ade2732fad72c19f40e5813ce6b8d85
Emanuele Giacomuzzo <emanuelegiacomuzzo@gmail.com>
2021-01-19 12:46:51 +0000

    a

--- Matlab_files/guidedClustering.m
+++ Matlab_files/guidedClustering.m
@@ -1,27 +1,10 @@
-function[A_clusters,centrality_clusters,clusterID,best_link,best_th]=guidedClustering(A,branches)
-
-best_W=0;
-for i=0.01:0.05:1 %i:link percentage
-    for j=0.01:0.05:1 %threshold
-        %if aggregation is one of the first two 
-        %    clusterID=cluster(branches ,'cutoff',i); 
-        %end
-    A_clusters=linkingClusters(A,clusterID,i); 
-    centrality_clusters=calculateCentrality(A_clusters,centrality_type); 
-    centrality_nodes=centralityNodes(centrality_clusters, clusterID);
-    centrality_compare=[centrality_original, centrality_nodes];
-    W = KendallCoef(centrality_compare);
-    if W > best_W 
-        best_W=W;
-        best_link_percentage=i; 
-    end
-    end
+function[A_clusters,centrality_nodes,clusterID,best_link,best_threshold]=guidedClustering(A,branches,aggregation)
+    
+[best_link,best_threshold] = bestLinkAndThreshold(A,clusterID,aggregation,branches);
+if aggregation=="Jaccard similarity HC" || aggregation=="Rege similarity HC"
+    clusterID=cluster(branches,'cutoff',best_threshold);
 end
-
-%if aggregation is one of the first two
-%    clusterID=cluster(branches,'cutoff',best_threshold);
-%end
-A_clusters=linkingClusters(A,clusterID,best_link_percentage);
+A_clusters=linkingClusters(A,clusterID,best_link);
 centrality_clusters=calculateCentrality(A_clusters,centrality_type);
 centrality_nodes=centralityNodes(centrality_clusters, clusterID);
 
