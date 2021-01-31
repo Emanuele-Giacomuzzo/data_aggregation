544a5f8d3ea7c497cbb6333dc7bc8ce661a6d49a
Emanuele Giacomuzzo <emanuelegiacomuzzo@gmail.com>
2021-01-31 16:18:35 +0000

    a

--- Matlab_files/visualise.m
+++ Matlab_files/visualise.m
@@ -2,10 +2,10 @@
 heatmap(membership);
 %Best parameters 
 heatmap(best_percentage);
-best_weight; %wired
+best_weight 
 %New centrality indices 
 heatmap(nDC);
-heatmap(nwDC);
+heatmap(nwDC); 
 heatmap(nCC);
 heatmap(nBC);
 heatmap(s);
@@ -24,21 +24,21 @@ heatmap(rs);
 
 
 %Centrality heatmap
-nDC_ratio=nDC/sum(nDC);
-nwDC_ratio=nwDC/sum(nwDC);
-nCC_ratio=nCC/sum(nCC);
-nBC_ratio=nBC/sum(nBC);
-s_ratio=s/sum(s);
-cs_ratio=cs/sum(cs);
-ns_ratio=ns/sum(ns);
-kindex_ratio=kindex/sum(kindex);
-kbu_ratio=kbu/sum(kbu);
-ktd_ratio=ktd/sum(ktd);
-kdir_ratio=kdir/sum(kdir);
-kindir_ratio=kindir/sum(kindir);
-TI_ratio=TI/sum(TI);
-STO_ratio=STO/sum(STO);
-TP_ratio=TP/sum(TP);
+nDC_ratio=nDC./sum(nDC);
+nwDC_ratio=nwDC./sum(nwDC);
+nCC_ratio=nCC./sum(nCC);
+nBC_ratio=nBC./sum(nBC);
+s_ratio=s./sum(s);
+cs_ratio=cs./sum(cs);
+ns_ratio=ns./sum(ns);
+kindex_ratio=kindex./sum(kindex);
+kbu_ratio=kbu./sum(kbu);
+ktd_ratio=ktd./sum(ktd);
+kdir_ratio=kdir./sum(kdir);
+kindir_ratio=kindir./sum(kindir);
+TI_ratio=TI./sum(TI);
+STO_ratio=STO./sum(STO);
+TP_ratio=TP./sum(TP);
 original_centralities=[nDC_ratio nwDC_ratio nCC_ratio nBC_ratio s_ratio cs_ratio ns_ratio kindex_ratio kbu_ratio ktd_ratio kdir_ratio kindir_ratio TI_ratio STO_ratio TP_ratio];
 h2=heatmap(centralities,node_names,original_centralities,'Colormap',parula);
 caxis(h2,[0, 1]);
