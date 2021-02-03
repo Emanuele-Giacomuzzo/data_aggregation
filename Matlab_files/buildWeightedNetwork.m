f612c39af8bc93072d7b3e17a36f83d41c3611a1
Emanuele Giacomuzzo <emanuelegiacomuzzo@gmail.com>
2021-02-03 09:23:52 +0000

    A

--- Matlab_files/buildWeightedNetwork.m
+++ Matlab_files/buildWeightedNetwork.m
@@ -1,76 +1,37 @@
 function [A_clustered] = buildWeightedNetwork(A,A_clustered,membership,centrality_type,weight_method)
 
-if (centrality_type=="nwDC" || centrality_type=="STO") && weight_method=="min"
+if (centrality_type=="nwDC" || centrality_type=="STO") 
     A_clustered_min=zeros(length(A_clustered));
+    A_clustered_max=zeros(length(A_clustered));
+    A_clustered_sum=zeros(length(A_clustered));
+    A_clustered_mean=zeros(length(A_clustered));
     for i=1:length(A_clustered)
         for j=1:length(A_clustered)
             MIN=10^10;
+            MAX=0;
+            SUM=0;
+            COUNT=0;
             for k=1:length(A)
                 for l=1:length(A)
-                    if membership(k)==i && membership(l)==j && A(k,l)~=0 && A(k,l)<MIN
-                        MIN=A(k,l);
+                    if membership(k)==i && membership(l)==j && A(k,l)~=0 
+                        if A(k,l)<MIN; MIN=A(k,l); end
+                        if A(k,l)>MAX; MAX=A(k,l); end
+                        SUM=SUM+A(k,l);
+                        COUNT=COUNT+1;
                     end
                 end
             end
             A_clustered_min(i,j)=MIN;
-        end
-        A_clustered = A_clustered_min;
-    end
-end
-
-if (centrality_type=="nwDC" || centrality_type=="STO") && weight_method=="mean"
-    A_clustered_mean=zeros(length(A_clustered));
-    for i=1:length(A_clustered)
-        for j=1:length(A_clustered)
-            COUNT=0;
-            SUM=0;
-            for k=1:length(A)
-                for l=1:length(A)
-                    if membership(k)==i && membership(l)==j && A(k,l)~=0
-                        COUNT=COUNT+1;
-                        SUM=SUM+A(k,l);
-                    end
-                end
-            end
+            A_clustered_max(i,j)=MAX;
+            A_clustered_sum(i,j)=SUM;
             A_clustered_mean(i,j)=SUM/COUNT;
         end
     end
-    A_clustered=A_clustered_mean;
+    if weight_method=="min"; A_clustered = A_clustered_min; end
+    if weight_method=="mean"; A_clustered=A_clustered_mean; end
+    if weight_method=="max"; A_clustered = A_clustered_max; end
+    if weight_method=="sum"; A_clustered = A_clustered_sum; end
 end
 
-if (centrality_type=="nwDC" || centrality_type=="STO") && weight_method=="max"
-    A_clustered_max=zeros(length(A_clustered));
-    for i=1:length(A_clustered)
-        for j=1:length(A_clustered)
-            MAX=0;
-            for k=1:length(A)
-                for l=1:length(A)
-                    if membership(k)==i && membership(l)==j && A(k,l)~=0 && A(k,l)>MAX
-                        MAX=A(k,l);
-                    end
-                end
-            end
-            A_clustered_max(i,j)=MAX;
-        end
-        A_clustered = A_clustered_max;
-    end
-end
-
-if (centrality_type=="nwDC" || centrality_type=="STO") && weight_method=="sum"
-    A_clustered_sum=zeros(length(A_clustered));
-    for i=1:length(A_clustered)
-        for j=1:length(A_clustered)
-            SUM=0;
-            for k=1:length(A)
-                for l=1:length(A)
-                    if membership(k)==i && membership(l)==j && A(k,l)~=0
-                        SUM=SUM+A(k,l);
-                    end
-                end
-            end
-        end
-        A_clustered = A_clustered_sum;
-    end
-end
 
 end
--- Matlab_files/possibleConnections.m
+++ Matlab_files/possibleConnections.m
@@ -1,12 +1,10 @@
 function[possible_connections]=possibleConnections(cluster_size)
 
-possible_connections=zeros(max(cluster_size));
-
+possible_connections=zeros(length(cluster_size));
 for i=1:length(cluster_size)
     for j=1:length(cluster_size)
         possible_connections(i,j)=cluster_size(i)*cluster_size(j);
     end
-    possible_connections(i,i)=0;
 end
 
 end
